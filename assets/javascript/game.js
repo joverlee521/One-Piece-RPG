8987987e8c94da0f893c8edf6ecf1d8cb9970ef0
J.Lee <joverlee521@gmail.com>
2018-08-23 14:38:34 -0700

    use .data to change the player's characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,22 @@
-var luffy = { "hp": 120, "attack": 10, "counter": 8};
-var buggy = { "hp": 100, "attack": 5, "counter": 3};
-var kuma = { "hp": 160, "attack": 12, "counter": 20};
-var doflamingo = { "hp": 200, "attack": 20, "counter": 18};
+$("#luffy").data({ "name": "Monkey D. Luffy", "hp": 120, "attack": 10, "counter": 8, "img": "assets/images/luffy.jpg"})
+$("#buggy").data({ "name": "Buggy The Clown", "hp": 100, "attack": 5, "counter": 3, "img": "assets/images/buggy.png"})
+$("#kuma").data({ "name": "Bartholomew Kuma", "hp": 160, "attack": 12, "counter": 20, "img": "assets/images/kuma.jpg"})
+$("#doflamingo").data({ "name": "Donquixote Doflaming", "hp": 200, "attack": 20, "counter": 18, "img": "assets/images/doflamingo.jpg"})
 
 var lockChar = false;
 var lockEnemy = false;
 
+
+
 $(document).ready(function(){
 
-    $("#luffy-hp").text(luffy.hp);
-    $("#buggy-hp").text(buggy.hp);
-    $("#kuma-hp").text(kuma.hp);
-    $("#doflamingo-hp").text(doflamingo.hp);
-
     $(".container-fluid").on("click", ".char-choice", function(){
-        var choosen = $(this).contents();
+        var choosen = $(this).data();
         if(lockChar !== true){
-            $("#player").replaceWith(choosen);
+            $("#player").css("visibility", "visible");
+            $("#player-name").text(choosen.name);
+            $("#player-img").attr("src", choosen.img);
+            $("#player-hp").text(choosen.hp);
             $(this).remove();
             lockChar = true;
         }
@@ -29,7 +29,6 @@ $(document).ready(function(){
 
     $(".btn").on("click", function(){
         if(lockChar == false || lockEnemy == false){
-            console.log("nothing happens")
             return;
         }
         else {
