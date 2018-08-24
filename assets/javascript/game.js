c82a50b410326c365bce13831168011b1c0b6546
J.Lee <joverlee521@gmail.com>
2018-08-23 21:23:24 -0700

    allow player to choose a new enemy when the first enemy runs out of HP

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,72 +2,118 @@
 $("#luffy").data({ "name": "Monkey D. Luffy", "hp": 120, "attack": 10, "counter": 8, "img": "assets/images/luffy.jpg"})
 $("#buggy").data({ "name": "Buggy The Clown", "hp": 100, "attack": 5, "counter": 3, "img": "assets/images/buggy.png"})
 $("#kuma").data({ "name": "Bartholomew Kuma", "hp": 160, "attack": 12, "counter": 20, "img": "assets/images/kuma.jpg"})
-$("#doflamingo").data({ "name": "Donquixote Doflaming", "hp": 200, "attack": 20, "counter": 18, "img": "assets/images/doflamingo.jpg"})
+$("#doflamingo").data({ "name": "Donquixote Doflamingo", "hp": 200, "attack": 20, "counter": 18, "img": "assets/images/doflamingo.jpg"})
 // Variable declarations
 var lockChar = false;
 var lockEnemy = false;
 var firstAttack = true;
+var newEnemy = false;
 var newPlayerHp = 0;
 var newDefenderHp = 0;
 var newPlayerAttack = 0;
-
-
-$(document).ready(function(){
-    // Clicking the characters cards
-    $(".container-fluid").on("click", ".char-choice", function(){
-        var choosen = $(this).data();
-        // Player choosing their character
-        if(lockChar !== true){
-            $("#player").data($(this).data());
-            $(".hidden1").css("visibility", "visible");
-            $("#player-name").text(choosen.name);
-            $("#player-img").attr("src", choosen.img);
-            $("#player-hp").text(choosen.hp);
-            $(this).remove();
-            // Prevents the player from choosing another character
-            lockChar = true;
-        }
-        // Player choosing their enemy
-        else if(lockEnemy !== true){
-            $("#defender").data($(this).data());
-            $(".hidden2").css("visibility", "visible");
-            $(".defender-name").text(choosen.name);
-            $("#defender-img").attr("src", choosen.img);
-            $("#defender-hp").text(choosen.hp);
-            $(this).remove();
-            // Prevents the player form choosing another defender
-            lockEnemy = true; 
-        }
-    });
-    // Clicking the attack button
-    $(".btn").on("click", function(){
+// The Game
+var rpg = {
+    startGame(){
+        // Clicking the characters cards
+        $(".container-fluid").on("click", ".char-choice", function(){
+            var choosen = $(this).data();
+            // Player choosing their character
+            if(lockChar !== true){
+                $("#player").data($(this).data());
+                $(".hidden1").css("visibility", "visible");
+                $("#player-name").text(choosen.name);
+                $("#player-img").attr("src", choosen.img);
+                $("#player-hp").text(choosen.hp);
+                $(this).remove();
+                // Prevents the player from choosing another character
+                lockChar = true;
+            }
+            // Player choosing their enemy
+            else if(lockEnemy !== true){
+                $(".hidden3").css("visibility", "hidden");
+                $(".hidden4").css("visiblity", "hiddne");
+                $(".hidden5").css("visibility", "hidden");
+                $("#defender").data($(this).data());
+                $(".hidden2").css("visibility", "visible");
+                $(".defender-name").text(choosen.name);
+                $("#defender-img").attr("src", choosen.img);
+                $("#defender-hp").text(choosen.hp);
+                $(this).remove();
+                // Prevents the player form choosing another defender
+                lockEnemy = true; 
+            }
+        });
+    },
+    inGame(){
         // Nothing happens when missing a player character and a defender
         if(lockChar == false || lockEnemy == false){
             return;
         }
         // First time the player attacks
         else if(firstAttack == true){
-            newPlayerHp = $("#player").data().hp - $("#defender").data().counter;
             newDefenderHp = $("#defender").data().hp - $("#player").data().attack;
+            newPlayerHp = $("#player").data().hp - $("#defender").data().counter;
             newPlayerAttack = $("#player").data().attack + $("#player").data().attack;
-            $(".hidden3").css("visibility", "visible");
+            $(".hidden3, .hidden4").css("visibility", "visible");
             $("#player-attack").text($("#player").data().attack);
             $("#defender-counter").text($("#defender").data().counter);
             $("#player-hp").text(newPlayerHp);
             $("#defender-hp").text(newDefenderHp);
             firstAttack = false;
         }
-        // All subsequent attacks
-        else{
+        // Switching to a new enemy
+        else if(newEnemy == true){
+            $(".hidden3").css("visibility", "visible");
+            $(".hidden5").css("visibility", "hidden");
+            newDefenderHp = $("#defender").data().hp - newPlayerAttack;
             newPlayerHp = newPlayerHp - $("#defender").data().counter;
-            newDefenderHp = newDefenderHp - newPlayerAttack;
+            $(".hidden4").css("visibility", "visible");
             $("#player-attack").text(newPlayerAttack);
             $("#defender-counter").text($("#defender").data().counter);
             $("#player-hp").text(newPlayerHp);
             $("#defender-hp").text(newDefenderHp);
             newPlayerAttack = newPlayerAttack + $("#player").data().attack;
+            newEnemy = false;
         }
+        // All subsequent attacks
+        else{
+            newDefenderHp = newDefenderHp - newPlayerAttack;
+            $("#defender-hp").text(newDefenderHp);
+            $("#player-attack").text(newPlayerAttack);
+            if(newDefenderHp > 0){
+                newPlayerHp = newPlayerHp - $("#defender").data().counter;
+                $("#defender-counter").text($("#defender").data().counter);
+                $("#player-hp").text(newPlayerHp);
+            }
+            else{
+                $(".hidden4").css("visibility", "hidden");
+                $(".hidden5").css("visibility", "visible");
+            }
+            newPlayerAttack = newPlayerAttack + $("#player").data().attack;
+        }
+    },
+    winRound(){
+        if(newDefenderHp <= 0){
+            lockEnemy = false;
+            newEnemy = true;
+            setTimeout(function(){
+                $(".hidden2").css("visibility", "hidden");
+            }, 1000)
+        }
+    },
+    loseGame(){
+        if(newDefenderHp > 0 && newPlayerHp <= 0){
+            $("#win-lose-text").text("YOU LOST!");
+            $("#win-lose-modal").modal({show: true, keyboard: false, backdrop: "static"});
+        }
+    }
+}
+$(document).ready(function(){
+    rpg.startGame();
+    // Clicking the attack button
+    $(".btn").on("click", function(){
+        rpg.inGame();
+        rpg.winRound();
+        rpg.loseGame();
     });
-    
-
 });
